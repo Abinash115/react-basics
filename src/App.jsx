c1e06ca5993aa367d6551c89af9a575276b8a326
Abinash115 <patsaniabinash@gmail.com>
2025-01-22 23:21:35 +0530

    Adding WelcomeMessage jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import FirstComponent from './components/react-component/FirstComponent'
 import Greet from './components/react-component/Greet'
+import WelcomeMessage from './components/react-jsx/WelcomeMessage'
 
 const App = () => {
   return (
     <div>
-      My First React App
+      {/* My First React App
       <FirstComponent/>
-      <Greet/>
+      <Greet/> */}
+      <WelcomeMessage/>
     </div>
   )
 }
