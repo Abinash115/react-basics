9cbc84a5990867b50ba2de4bffad9d9a07c43d6e
Abinash115 <patsaniabinash@gmail.com>
2025-01-23 08:05:18 +0530

    Adding Jsx Rules Files

---
+++ src/components/react-jsx/JsxRules.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const JsxRules = () => {
+  return (
+    <div>
+      <h1>Jsx Rules</h1>
+      <p>
+        <p>Single Parent element</p>
+        <br /> 
+        {/* Enclosing tag */}
+        <p className='hello'>Hi Abinash</p>
+      </p>
+    </div>
+  )
+}
+
+export default JsxRules
